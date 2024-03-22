43582a93307cdfe1bff768e044d6678ea1bf92d2
samuelmaderson <samuel.maderson@gmail.com>
2024-03-21 22:55:05 -0300

    the creation of whole vpc infrastructure has been finished

--- src/vpc/vpc.go
+++ src/vpc/vpc.go
@@ -121,3 +121,119 @@ func CreatePrivateSubnets(sess *session.Session, vpcID string, subnet_1 string, 
 
 	return subnets
 }
+
+func CreateNatGateway(sess *session.Session, subnetID string) string {
+
+	svc := ec2.New(sess)
+
+	elasticIpInput := &ec2.AllocateAddressInput{
+		Domain: aws.String("vpc"),
+	}
+	elasticIpResult, err := svc.AllocateAddress(elasticIpInput)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	natGatewayInput := &ec2.CreateNatGatewayInput{
+		AllocationId: elasticIpResult.AllocationId,
+		SubnetId:     aws.String(subnetID),
+	}
+	natGatewayResult, err := svc.CreateNatGateway(natGatewayInput)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return *natGatewayResult.NatGateway.NatGatewayId
+}
+
+func CreateInternetGateway(sess *session.Session, vpcID string) string {
+
+	svc := ec2.New(sess)
+
+	internetGatewayInput := &ec2.CreateInternetGatewayInput{}
+	internetGatewayResult, err := svc.CreateInternetGateway(internetGatewayInput)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	attachGatewayInput := &ec2.AttachInternetGatewayInput{
+		InternetGatewayId: internetGatewayResult.InternetGateway.InternetGatewayId,
+		VpcId:             aws.String(vpcID),
+	}
+	_, err = svc.AttachInternetGateway(attachGatewayInput)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return *internetGatewayResult.InternetGateway.InternetGatewayId
+}
+
+func CreatePrivateRouteTable(sess *session.Session, vpcID string, ngwID string, subnets []string) {
+
+	svc := ec2.New(sess)
+
+	routeTableOutput, err := svc.CreateRouteTable(&ec2.CreateRouteTableInput{
+		VpcId: aws.String(vpcID),
+	})
+	if err != nil {
+		log.Fatalf("Error creating Route Table: %v", err)
+	}
+
+	routeTableID := routeTableOutput.RouteTable.RouteTableId
+
+	_, err = svc.CreateRoute(&ec2.CreateRouteInput{
+		RouteTableId:         aws.String(*routeTableID),
+		DestinationCidrBlock: aws.String("0.0.0.0/0"),
+		GatewayId:            aws.String(ngwID),
+	})
+	if err != nil {
+		log.Fatalf("Error adding route to Route Table: %v", err)
+	}
+
+	for _, subnetID := range subnets {
+		_, err = svc.AssociateRouteTable(&ec2.AssociateRouteTableInput{
+			RouteTableId: aws.String(*routeTableID),
+			SubnetId:     aws.String(subnetID),
+		})
+		if err != nil {
+			log.Fatalf("Error associating subnet with Route Table: %v", err)
+		}
+	}
+}
+
+func UpdatePublicRouteTable(sess *session.Session, vpcID string, igwID string, subnets []string) {
+
+	svc := ec2.New(sess)
+
+	resp, err := svc.DescribeRouteTables(&ec2.DescribeRouteTablesInput{})
+	if err != nil {
+		fmt.Println("failed to describe route tables,", err)
+		return
+	}
+
+	routeTableID := *resp.RouteTables[1].RouteTableId
+
+	_, err = svc.CreateRoute(&ec2.CreateRouteInput{
+		RouteTableId:         aws.String(routeTableID),
+		DestinationCidrBlock: aws.String("0.0.0.0/0"),
+		GatewayId:            aws.String(igwID),
+	})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for _, subnetID := range subnets {
+
+		_, err = svc.AssociateRouteTable(&ec2.AssociateRouteTableInput{
+			RouteTableId: aws.String(routeTableID),
+			SubnetId:     aws.String(subnetID),
+		})
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+	}
+
+}
